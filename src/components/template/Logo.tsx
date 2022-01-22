54d84b85917a64222f38f0a59920d1e2140403fd
Fabio Dias <mfabiodias@gmail.com>
2022-01-22 12:36:39 -0300

    Added logo SVG and PNG types

--- src/components/template/Logo.tsx
+++ src/components/template/Logo.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import { IconLogo } from "../icons";
+import iconLogoPNG from '../../../public/images/logo.png'
+
 export default function Logo() {
     return (
         <div className={`
@@ -5,11 +9,18 @@ export default function Logo() {
             h-12 w-12 rounded-full
             bg-white
         `}>
-            <div className="h-3 w-3 rounded-full bg-red-600 mb-0.5" />
+            {/* CSS ONLY */}
+            {/* <div className="h-3 w-3 rounded-full bg-red-600 mb-0.5" />
             <div className="flex mt-0.5">
                 <div className="h-3 w-3 rounded-full bg-yellow-500 mr-0.5" />
                 <div className="h-3 w-3 rounded-full bg-green-600 ml-0.5" />
-            </div>
+            </div> */}
+
+            {/* Com SVG */}
+            {IconLogo()}
+
+            {/* Com file PNG/JPG/GIF */}
+            {/* <Image src={iconLogoPNG} width="32" height="32" /> */}
         </div>
     )
 }
